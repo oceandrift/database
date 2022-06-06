942f9e554328d2e34d1778730e2b6c80716521dd
Elias Batek <desisma@heidel.beer>
2022-06-06 04:57:47 +0200

    Begin MariaDB driver implementation

--- source/oceandrift/db/mariadb.d
+++ source/oceandrift/db/mariadb.d
@@ -1,28 +1,84 @@
 module oceandrift.db.maridb;
 
+import mysql.safe;
 import oceandrift.db.dbal;
 
-import mysql.safe;
-
-class MariaDBDatabaseDriver : DatabaseDriver
+class MariaDBDatabaseDriver : oceandrift.db.dbal.DatabaseDriver
 {
 @safe:
 
     private
     {
         Connection _connection;
+
+        string _host;
+        ushort _port;
+        string _username;
+        string _password;
+        string _database;
     }
 
     public this(string host, ushort port, string username, string password, string database)
     {
-        this._connection = new Connection();
+        this._host = host;
+        this._port = port;
+        this._username = username;
+        this._password = password;
+        this._database = database;
     }
 
     public  // DatabaseDriver
     {
+        void connect()
+        {
+            this._connection = new Connection(
+                this._host,
+                this._username,
+                this._password,
+                this._database,
+                this._port
+            );
+        }
+
+        void close()
+        {
+            this._connection.close();
+        }
+
         bool connected()
         {
-            return true;
+            return ((this._connection !is null)
+                    && !this._connection.closed);
+        }
+
+        bool autoCommit()
+        {
+            return this._connection
+                .queryRow("SHOW VARIABLES LIKE 'autocommit'")
+                .get[1] == "ON";
+        }
+
+        void autoCommit(bool enable)
+        {
+            if (enable)
+                this._connection.exec("SET autocommit=1");
+            else
+                this._connection.exec("SET autocommit=0");
+        }
+
+        void transactionStart()
+        {
+            this._connection.exec("START TRANSACTION");
+        }
+
+        void transactionCommit()
+        {
+            this._connection.exec("COMMIT");
+        }
+
+        void transactionRollback()
+        {
+            this._connection.exec("ROLLBACK");
         }
     }
 
